005cd304d241441fe03dff885fee7383075f1f51
prasad <aprasadreddy9@gmail.com>
2025-09-10 15:28:45 +0530

    Postgre Sql connected successfully

--- Application/settings.py
+++ Application/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Hello.apps.HelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'Application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Freelancers',
+        'USER' : 'postgres',
+        'PASSWORD':'Prasad',
+        'HOST':'localhost'
+        
     }
 }
 
--- Hello/models.py
+++ Hello/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Freelancer_data(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    skills = models.TextField()
