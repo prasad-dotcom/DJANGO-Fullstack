b1fa79a96789831ec5287e172147909059b950bf
prasad <aprasadreddy9@gmail.com>
2025-10-04 10:22:22 +0530

    Merge remote changes

--- Application/settings.py
+++ Application/settings.py
@@ -175,7 +175,7 @@ DEFAULT_FROM_EMAIL = 'zimbeam2499@gmail.com'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     
 
