f8c4cb1611b0acf286fd90bf4e9ce52270945f51
prasad <aprasadreddy9@gmail.com>
2025-10-16 03:27:34 +0530

    migrations

--- Application/settings.py
+++ Application/settings.py
@@ -213,8 +213,8 @@ CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:8080",
     "http://127.0.0.1:9000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
 ]
 
 CORS_ALLOW_METHODS = (
